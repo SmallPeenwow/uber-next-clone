f66ceb20cac72b436152da001d684da17ac6f0cc
SmallPeenwow <bowdencamryn@gmail.com>
2023-10-26 00:04:43 +0200

    Mapbox GL Added Into Project

--- app/page.js
+++ app/page.js
@@ -1,5 +1,42 @@
-import Image from 'next/image';
+'use client';
+
+import tw from 'tailwind-styled-components';
+import mapboxgl from 'mapbox-gl';
+import { useEffect } from 'react';
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 export default function Home() {
-	return <main className='flex min-h-screen flex-col py-0 px-8'></main>;
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [0, 0],
+			zoom: 3,
+		});
+	}, []);
+
+	// const map = new mapboxgl.Map({
+	// 	container: 'map',
+	// 	style: 'mapbox://styles/mapbox/streets-v11',
+	// });
+
+	return (
+		<Wrapper>
+			<Map id='map'></Map>
+			<ActionItems>Map</ActionItems>
+		</Wrapper>
+	);
 }
+
+const Wrapper = tw.main`
+	flex flex-col bg-red-300 h-screen
+`;
+
+const Map = tw.div`
+flex-1 bg-red-500
+`;
+
+const ActionItems = tw.div`
+flex-1
+`;
